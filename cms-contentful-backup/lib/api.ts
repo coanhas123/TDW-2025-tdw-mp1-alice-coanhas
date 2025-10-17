82f3244e71ccf67df06a64bb25b0b3c4bef3f21d
coanhas123 <131986830+coanhas123@users.noreply.github.com>
2025-10-17 21:16:37 +0100

    apagar backup

--- cms-contentful-backup/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import { contentfulClient } from "./contentful";
-
-export async function getPosts() {
-  const response = await contentfulClient.getEntries({
-    content_type: "post",
-    include: 2, // inclui o autor dentro do post
-  });
-  return response.items;
-}
--- cms-contentful-backup/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    domains: ["images.ctfassets.net"], // necessário para imagens do Contentful
-  },
-};
-
-module.exports = nextConfig;
