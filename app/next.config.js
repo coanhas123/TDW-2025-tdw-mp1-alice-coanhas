7a188e9221fbb524322abba60220f6f9c9cfe5ba
coanhas123 <131986830+coanhas123@users.noreply.github.com>
2025-10-17 20:55:40 +0100

    Workflow simplicar codigo

---
+++ app/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.ctfassets.net",
+      },
+      {
+        protocol: "https",
+        hostname: "**.contentful.com",
+      },
+    ],
+  },
+};
+module.exports = {
+  output: 'export',
+};
+
+module.exports = nextConfig;
